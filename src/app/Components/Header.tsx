ed3555af6b813e145891f7745e5f107c569b8454
Samreen001 <164864743+Samreen001@users.noreply.github.com>
2024-10-14 21:39:51 +0500

    Initial commit

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-cyan-400 text-center justify-center'>
-      <h1> My First Web Page</h1>
+    <div className='bg-cyan-400 cursor pointer text-center justify-center'>
+      <h1> <b>My First Web Page</b></h1>
     </div>
   )
 }
